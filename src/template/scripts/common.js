6ff0cba70b23c95c3f98d9e46a6eaa81f036cfc2
e5ash <io@e5ash.com>
2018-03-16 20:55:33 +0300

    85%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,35 @@ $(document).ready(function($) {
 
 	$('.text_phone').mask('+7 (999) 999-99-99');
 	
+
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true
+	})
+
+	$('select').styler({
+		selectSmartPositioning: false
+	});
+
+	var productImg = $('.product__img'),
+			productImgs = $('.product__imgs');
+
+	productImg.slick({
+		arrows: false,
+		fase: true,
+		asNavFor: productImgs
+	});
+
+	productImgs.slick({
+		arrows: false,
+		focusOnSelect: true,
+		asNavFor: productImg,
+		slidesToShow: 3
+	})
+
+	$('.product__amount li').click(function(event) {
+		$('.product__amount li').removeClass('selected');
+		$(this).addClass('selected');
+	});
 });
